84c46e768ccb897cfcd881ab9949c0aedc0f2693
Ali Nosrati <nosrati.aa@gmail.com>
2016-12-11 01:05:41 -0600

    The result directory added and the redfish-monitoring-not-raw.sh script changed to match with new structure

--- bin/redfish-monitoring-not-raw.sh
+++ bin/redfish-monitoring-not-raw.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
+
+#----------Name of the results' directory for the current run----------------------
+#----------Used the current date and time to make it unique------------------------
 date=`date +%F`
 hour=`date +%H`
 minute=`date +%M`
 second=`date +%S`
 runTime=$date-$hour-$minute-$second
 
-#testNumber=$(cat /root/Ali/redfish/tests/redfish-tests.info | wc -l) 
+#----------Setting the results' directory path-------------------------------------
+if [ $1 ]
+then
+    resultsPath=$1
+else
+    resultsPath="../results/not-raw"
+fi
+#echo $resultsPath
 
-#for rackNumber in `cat /root/Ali/redfish/tests/cluster.info | awk '{print $1}' | tail -n +2`
-#do
-#    min=`cat /root/Ali/redfish/tests/cluster.info | grep $rackNumber | awk '{print $2}'`
-#    max=`cat /root/Ali/redfish/tests/cluster.info | grep $rackNumber | awk '{print $3}'`
-#    for i in `seq 2 $testNumber`
-#    do
-#        testName=`cat /root/Ali/redfish/tests/redfish-tests.info | awk '{print $1}' | head -n +$i | tail -1`
-#        testCommand=`cat /root/Ali/redfish/tests/redfish-tests.info | awk -F $'\t' '{print $2}' | head -n +$i | tail -1`
-#        echo "(time /root/Ali/redfish/tests/run-not-raw-parallel-test.sh $runNumber $rackNumber $min $max $testName "$testCommand") &> /root/Ali/redfish/results/parallel/not-raw/run-$runNumber/$testName/rack$rackNumber-$testName.time &"
-#    done
-#    wait
-#done
+#----------Finding number of tags for the upcoming tests---------------------------
+tagNumber=$(cat ../config/redfish-subcommands.conf | sed -n -e '/---/,$p' | wc -l)
 
-#!/bin/bash
-
-#--------------------------------------------
-#runNumber=$1
-#rack=$2
-#min=$3
-#max=$4
-#testName=$5
-#testCommand=$6
-#
-#for i in `seq $min $max`
-#do
-#            (time python3.4 redfishtool.py -r 10.101.$rack.$i -SAlways -u root -p calvin -ss -vvv Chassis -1 $testCommand) &> /root/Ali/redfish/results/parallel/not-raw/run-$runNumber/$testName/$rack-$i &
-#        done
-#        wait
-#        ~   
+#----------Extracting the subcommands and their tags from the config file---------- 
+for i in `seq 2 $tagNumber`
+do
+    tagName=`cat ../config/redfish-subcommands.conf | sed -n -e '/---/,$p' | awk '{print $1}' | head -n +$i | tail -1`
+    subCommand=`cat ../config/redfish-subcommands.conf | sed -n -e '/---/,$p' |awk -F $'\t' '{print $2}' | head -n +$i | tail -1`
+#    echo $subCommand
+    
+#----------Gathering data for each tag from all cluster nodes----------------------
+    for node in `cat ../config/cluster.conf | sed -e '1,/--/d' | awk '{print $3}'`
+    do
+        rackID=`cat ../config/cluster.conf | grep -w $node | awk -F$'\t' '{print $1}'`
+        nodeID=`cat ../config/cluster.conf | grep -w $node | awk -F$'\t' '{print $2}'`
+        echo "(time python3.4 ../tools/Redfishtool/redfishtool.py -r $node -SAlways -u root -p calvin -ss -vvv $subCommand) &> $resultsPath/$runTime/$tagName/$rackID/$rackID-$nodeID &"
+    done
+done
